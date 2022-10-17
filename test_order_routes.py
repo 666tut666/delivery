65c38c2a2a907a9e486b5d587b2becba1a1a81c2
666tut666 <666tut666@users.noreply.github.com>
2022-10-17 10:42:47 +0545

    Add files via upload

--- test_order_routes.py
+++ test_order_routes.py
@@ -4,8 +4,8 @@ from httpx import AsyncClient
 
 
 @pytest.mark.anyio
-async def test_hello(self):
-    async with AsyncClient(app=app, base_url="http://test/orders"):
-        response = self.client.get('/orders')
+async def test_hello():
+    async with AsyncClient(app=app, base_url="http://test/orders") as ac:
+        response = await ac.get('/orders')
     assert response.status_code == 200
     assert response.json() == {"message":"order"}
