fd5ebf9ab00d2cbada990de66de2c79f2727fdf4
666tut666 <666tut666@users.noreply.github.com>
2022-10-18 08:59:52 +0545

    3rd commit
    third

--- auth_routes.py
+++ auth_routes.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+from fastapi.exceptions import HTTPException
 from database import Session, engine
-from schemas import SignUpModel
+from schemas import SignUpModel, LoginModel
 from fastapi.exceptions import HTTPException
 from werkzeug.security import generate_password_hash, check_password_hash
+from models import User
+from fastapi_jwt_auth import AuthJWT
+from fastapi.encoders import jsonable_encoder
 
 
 auth_router=APIRouter(
@@ -15,11 +19,21 @@ session=Session(bind=engine)
 
 
 @auth_router.get('/')
-async def hello():
+async def hello(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+              detail="Invalid token"
+        )
+
+
+
     return{"message":"auth"}
 
 
-@auth_router.post('/signup', response_model=SignUpModel,
+@auth_router.post('/signup',
                   status_code=status.HTTP_201_CREATED
 )
 async def signup(user:SignUpModel):
@@ -50,3 +64,43 @@ async def signup(user:SignUpModel):
     session.commit()
 
     return new_user
+
+
+##login route
+
+@auth_router.post('/login',status_code=200)
+async def login(user:LoginModel, Authorize:AuthJWT=Depends()):
+    db_user=session.query(User).filter(User.username==user.username).first()
+
+    if db_user and check_password_hash(db_user.password, user.password):
+        access_token= Authorize.create_access_token(subject=db_user.username)
+        refresh_token= Authorize.create_refresh_token(subject=db_user.username)
+
+        response={
+            "access":access_token,
+            "refresh":refresh_token
+        }
+
+        return jsonable_encoder(response)
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid name or password"
+    )
+
+#refreshing tokens
+
+@auth_router.get('/refresh')
+async def refresh_token(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_refresh_token_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="please provide a valid refresh Token"
+        )
+
+    current_user=Authorize.get_jwt_subject()
+
+    access_token=Authorize.create_access_token(subject=current_user)
+
+    return jsonable_encoder({"access":access_token})
--- schemas.py
+++ schemas.py
@@ -7,7 +7,7 @@ class SignUpModel(BaseModel):
     email:str
     password:str
     is_staff:Optional[bool]
-    is_active:Optional[bool ]
+    is_active:Optional[bool]
 
 
     class Config:
@@ -18,6 +18,32 @@ class SignUpModel(BaseModel):
                 "email":"user@delivery.com",
                 "password":"password",
                 "is_staff":False,
-                "is_active":False
+                "is_active":True
+            }
+        }
+
+class Settings(BaseModel):
+    authjwt_secret_key:str='47e4dc053cd133d12abd992816e285e10e590627ad32b22257dc178320a2489e'
+
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
             }
         }
